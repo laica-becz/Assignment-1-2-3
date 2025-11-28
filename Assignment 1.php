be20bb65fa01ff088393d7fd55d69295d5f0b0ac
laica-becz <zepedabiancaellise@gmail.com>
2025-11-28 22:07:07 +0800

    Update Assignment 1.php

--- Assignment 1.php
+++ Assignment 1.php
@@ -1,9 +1,20 @@
-<?php 
+<?php
+//variables
 $product_name = "No Bake Brownies";
-$ingredients = ["1 cup almond butter", "1/2 cup maple syrup", "2/3 cup cocoa powder", "1 cup chocolate frosting"];
 $prep_time = "5 mins";
 $serving_size = 9;
+$difficulty = "Easy"; 
+//indexed array
+$ingredients = ["1 cup almond butter", "1/2 cup maple syrup", "2/3 cup cocoa powder", "1 cup chocolate frosting"];
+//associative array
+$steps = ["step 1" => "Line a square pan with parchment paper and set aside.", "step 2" => "In a large mixing bowl, add your 
+almond butter, maple syrup, and cocoa powder and mix well", "step 3" => "Transfer the batter into the lined pan and press down and smooth out.
+Add your frosting on top and refrigerate it for at least 30 minutes.", "step 4" => "Remove the no bake brownies from the refrigerator
+and slice and serve."];
+
+//expression and operators
 $total = $prep_time . " " . $serving_size;
+
 ?>
 
 <!DOCTYPE html>
@@ -16,4 +27,5 @@ $total = $prep_time . " " . $serving_size;
 <body>
     
 </body>
+
 </html>
