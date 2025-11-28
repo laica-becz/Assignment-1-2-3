80cfe4719523d1664d412d2a3c8cfc0a9545ce56
laica-becz <zepedabiancaellise@gmail.com>
2025-11-28 21:47:23 +0800

    Add files via upload

---
+++ Assignment 1.php
@@ -0,0 +1,19 @@
+<?php 
+$product_name = "No Bake Brownies";
+$ingredients = ["1 cup almond butter", "1/2 cup maple syrup", "2/3 cup cocoa powder", "1 cup chocolate frosting"];
+$prep_time = "5 mins";
+$serving_size = 9;
+$total = $prep_time . " " . $serving_size;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How to make no bake Brownies</title>
+</head>
+<body>
+    
+</body>
+</html>
