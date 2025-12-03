fac93961ee7a3e356d7a0f331a1ab94be85159f5
laica-becz <zepedabiancaellise@gmail.com>
2025-12-03 19:53:01 +0800

    Create Assign 3.php

---
+++ Assign 3.php
@@ -0,0 +1,11 @@
+<?php
+//Code Sequence
+declare(strict_types=1); //Step 1
+
+$ingredients = ['Almond Butter' => ['price' => 390.00, 'stock' => 10],
+                'Cocoa Powder' => ['price' => 203.00, 'stock' => 5],
+                'Maple Syrup' => ['price' => 200.00, 'stock' => 4],
+                'Chocolate Frosting' => ['price' => 100.00, 'stock' => 7]
+            ]; //Step 2
+//Step 3
+$tax_rate = 0.12; //12% VAT 
